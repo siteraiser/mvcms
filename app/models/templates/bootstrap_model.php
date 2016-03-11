c6fba8d4e862b0095bfd3ff38b001e5dbae33394
Carl Turechek <carl@siteraiser.com>
2016-03-11 18:06:59 -0500

    Update bootstrap_model.php

--- app/models/templates/bootstrap_model.php
+++ app/models/templates/bootstrap_model.php
@@ -1,4 +1,22 @@
-<?php 
+<?php
+/*	Copyright © 2016 
+	
+	This file is part of MVCMS.
+
+    MVCMS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MVCMS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MVCMS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 class bootstrap_model extends requestHandler{
 	public function respImgs($html){
