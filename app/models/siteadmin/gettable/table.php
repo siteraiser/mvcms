caeeb9d47d1afdda3178b8bdab6d9b1c2ca2b088
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:21:10 -0500

    Update table.php

--- app/models/siteadmin/gettable/table.php
+++ app/models/siteadmin/gettable/table.php
@@ -1,4 +1,18 @@
-<?php 
+<?php /*
+Copyright © 2016 
+	
+	This file is part of PHP-MVCMS.
+    PHP-MVCMS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    PHP-MVCMS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with MVCMS.  If not, see <http://www.gnu.org/licenses/>.
+*/	
 class table extends requestHandler{
 public $head='';
 public $rows='';
