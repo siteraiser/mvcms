0a8fa41338bce510d6a529d5b7267afc7a760f30
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:39:13 -0500

    Update blog.php

--- app/controllers/blog.php
+++ app/controllers/blog.php
@@ -15,7 +15,7 @@ Copyright © 2016
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with MVCMS.  If not, see <http://www.gnu.org/licenses/>.
+    along with PHP-MVCMS.  If not, see <http://www.gnu.org/licenses/>.
 */	
 class blog extends requestHandler{
 	public function index($link){
