08f506434cfb749c30a8c32aa1d7a2e677d5e0cf
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:23:13 -0500

    Update pages_model.php

--- app/models/siteadmin/pages_model.php
+++ app/models/siteadmin/pages_model.php
@@ -1,18 +1,15 @@
 <?php /*
-Copyright © 2016 
+Copyright Â© 2016 
 	
-	This file is part of MVCMS.
-
-    MVCMS is free software: you can redistribute it and/or modify
+	This file is part of PHP-MVCMS.
+    PHP-MVCMS is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
-    MVCMS is distributed in the hope that it will be useful,
+    PHP-MVCMS is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with MVCMS.  If not, see <http://www.gnu.org/licenses/>.
 */	
