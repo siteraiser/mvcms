d2a60a4bfe8e808a470b608d4d2a6ef9462ee979
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:16:32 -0500

    Update blog.php

--- app/controllers/blog.php
+++ app/controllers/blog.php
@@ -2,14 +2,14 @@
 /*
 Copyright © 2016 
 	
-	This file is part of MVCMS.
+	This file is part of PHP-MVCMS.
 
-    MVCMS is free software: you can redistribute it and/or modify
+    PHP-MVCMS is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    MVCMS is distributed in the hope that it will be useful,
+    PHP-MVCMS is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
