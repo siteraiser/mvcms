1d625aa05aae3b44b06572f6eedf1da651db0cb5
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:51:18 -0500

    Update article.php

--- app/views/siteadmin/article.php
+++ app/views/siteadmin/article.php
@@ -13,7 +13,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with MVCMS.  If not, see <http://www.gnu.org/licenses/>.
+    along with PHP-MVCMS.  If not, see <http://www.gnu.org/licenses/>.
 */
 ?>
 <div class="container-fluid">
