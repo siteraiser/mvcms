3afd5dfb345c71d2720122520dce6707489cb467
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:42:14 -0500

    Update categories_model.php

--- app/models/siteadmin/categories_model.php
+++ app/models/siteadmin/categories_model.php
@@ -11,7 +11,7 @@ Copyright © 2016
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with MVCMS.  If not, see <http://www.gnu.org/licenses/>.
+    along with PHP-MVCMS.  If not, see <http://www.gnu.org/licenses/>.
 */		
 class categories_model extends requestHandler{
 	public function getCategoryById($id=''){
