2edb6e470e47c1f4f866993ee321fa6ed9e2012a
Carl Turechek <carl@siteraiser.com>
2016-03-12 19:09:03 -0500

    Added viewport to bootstrap template head.

--- app/views/templates/bootstrap/header.php
+++ app/views/templates/bootstrap/header.php
@@ -3,6 +3,7 @@
     <head>
     <title><?php if(isset($title))echo$title;?></title>
 	<?php if(isset($meta))echo$meta;?>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/assets/css/dropmenu.css">
 	<link rel="stylesheet" href="/assets/css/sidemenu.css">
