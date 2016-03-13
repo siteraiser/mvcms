5632e4cfec6dab0f7980fe3d81dd3edd7b86e68a
Carl Turechek <carl@siteraiser.com>
2016-03-12 20:54:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-/*	Copyright © 2016 
+/*	PHP-MVCMS - Copyright © 2016 
 
     PHP-MVCMS is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
