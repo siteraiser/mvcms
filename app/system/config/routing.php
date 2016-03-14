d3135c91647ee73eb348460127688f09f55a6ad9
Carl Turechek <carl@siteraiser.com>
2016-03-14 14:06:02 -0400

    Ordering of arguments was added to routing.

--- app/system/config/routing.php
+++ app/system/config/routing.php
@@ -5,5 +5,5 @@ $rows['blog/login'] = 'blog/login';
 $rows['blog/logout'] = 'blog/logout';
 $rows['blog/admin'] = 'blog/admin';
 $rows['blog/admin/(:any)'] = 'blog/admin/$1';
-$rows['blog/(:any)/(:any)'] = 'blog/article';
-$rows['blog/(:any)'] = 'blog/category';
+$rows['blog/(:any)/(:any)'] = 'blog/article/$1/$2';
+$rows['blog/(:any)'] = 'blog/category/$1';
